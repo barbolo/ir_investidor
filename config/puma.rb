e6d2da82223239ac5ba4d44bed9db15a8cf0457e
Rafael Barbolo <rafael.barbolo@infosimples.com.br>
2018-11-30 11:31:06 -0200

    Setup Puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,30 @@
+# Load environment variables
+env_config = {
+  'rails_env'   => ENV.fetch('RAILS_ENV') { 'development' },
+  'threads_min' => ENV.fetch("WEB_THREADS_MIN") { 5 }.to_i,
+  'threads_max' => ENV.fetch("WEB_THREADS_MAX") { 5 }.to_i,
+  'web_port'    => ENV.fetch("WEB_PORT") { 3000 }.to_i,
+}
+
+# Find the Rails application root directory
+app_dir = File.expand_path("../..", __FILE__)
+directory app_dir
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+threads env_config['threads_min'], env_config['threads_max']
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port env_config['web_port']
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment env_config['rails_env']
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -31,4 +42,4 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# plugin :tmp_restart
